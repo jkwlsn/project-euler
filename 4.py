f7038e875ab5ce6c271b5d6029ea1c8624ea5bb0
Jake Wilson <hi@jkwlsn.dev>
2025-07-17 13:39:41 +0100

    Improves performance
    Prevent duplicate products by starting the second loop at the first number.
    
    Break if the product is smaller than the largest product, this means at least one factor is too small.
    
    Adds performance checks using `time` module

--- 4.py
+++ 4.py
@@ -4,21 +4,27 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 
 Find the largest palindrome made from the product of two **3**-digit numbers."""
 
+import time
+
 
 def largest_palindrome_product() -> int:
-    max_ = range(999, 99, -1)
     max_palindrome = 0
     factors = ()
-    for x in max_:
-        for y in max_:
+    for x in range(999, 99, -1):
+        for y in range(x, 99, -1):
             product = x * y
-            if product >= max_palindrome:
-                if str(product) == str(product)[::-1]:
-                    max_palindrome = product
-                    factors = (x, y)
+            if product <= max_palindrome:
+                break
+            if str(product) == str(product)[::-1]:
+                max_palindrome = product
+                factors = (x, y)
     print(max_palindrome, factors)
     return max_palindrome
 
 
 if __name__ == "__main__":
+    start_time = time.perf_counter()
     largest_palindrome_product()
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.6f} seconds")
